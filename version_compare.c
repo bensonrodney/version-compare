a0d5f092bad0bdff8e1889a1a150d2a1012b259b
Jason Milen <jason.milen@opengear.com>
2019-06-22 13:57:40 +1000

    added demo version of version comparison, which should be made into a library package later

---
+++ version_compare.c
@@ -0,0 +1,201 @@
+/* version_compare.c - Jason Milen 2019
+ * Demo for use as a version string equality, inequality,
+ * greater than, less than, greater or equal, less or equal,
+ * or combos of those. See the assertion tests in main().
+ */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
+typedef struct {
+	uint8_t num_sections;
+	int *sections;
+} version_t;
+
+version_t *new_version() {
+	version_t *v = (version_t *)malloc(sizeof(version_t));
+	if (!v) return NULL;
+	v->num_sections = 0;
+	return v;
+}
+
+int free_version(version_t *v) {
+	if (!v) return 1;
+	if (v->sections) free(v->sections);
+	free(v);
+	return 1;
+}
+
+version_t *str_to_ver(const char *verstr) {
+	version_t *v = new_version();
+	char *dup = strdup(verstr);
+	char *remain = dup;
+	char *token = NULL;
+	int subv = 0;
+
+	while ((token = strtok_r(remain, ".", &remain))) {
+		subv = atoi(token);
+		if (v->num_sections > 0) {
+			v->sections = (int *) realloc(v->sections, (v->num_sections + 1) * sizeof(int));
+		} else {
+			v->sections = (int *) calloc(1, sizeof(int));
+		}
+
+		v->sections[v->num_sections++] = subv;
+	}
+	if (dup) free (dup);
+	return v; 
+}
+
+int min_sections(const version_t *v1, const version_t *v2) {
+	if (!v1 || !v2) return 0;
+	return v1->num_sections < v2->num_sections ? v1->num_sections : v2->num_sections; 
+}
+
+// used at the start of a comparison function that takes
+// two strings as the versions
+#define CONVERT_VERSION_STRINGS \
+	version_t *v1 = str_to_ver(version1); \
+	version_t *v2 = str_to_ver(version2); \
+	if (!v1 || !v2) { \
+		free_version(v1); \
+		free_version(v2); \
+		return -1; \
+	} \
+
+// used at the end of a comparison funtion that takes two
+// strings as the versions
+#define COMP_END \
+cleanup: \
+	free_version(v1); \
+	free_version(v2); \
+	return rv; \
+
+int vt_eq(const version_t *v1, const version_t *v2) {
+	int rv = 0;
+	int sections = min_sections(v1, v2);
+	if (v1->num_sections != v2->num_sections) return 0;
+	for (int i=0; i < sections; i++) {
+		if (v1->sections[i] != v2->sections[i]) return 0;
+	}
+	return 1;
+}
+
+int vt_ne(const version_t *v1, const version_t *v2) {
+	int rv = vt_eq(v1, v2);
+	if (rv < 0) return rv;
+	return rv == 1 ? 0 : 1;
+}
+
+int vt_gt(const version_t *v1, const version_t *v2) {
+	int sections = min_sections(v1, v2);
+	for (int i=0; i < sections; i++) {
+		if (v1->sections[i] > v2->sections[i]) return 1;
+		if (v1->sections[i] < v2->sections[i]) return 0;
+	}
+	if (v1->num_sections > v2->num_sections) return 1;
+	return 0;
+}
+
+int vt_ge(const version_t *v1, const version_t *v2) {
+	int rv = vt_eq(v1, v2);
+	if (rv == -1) return -1;
+	if (rv == 1) return 1;
+	return vt_gt(v1, v2);
+}
+
+int vt_lt(const version_t *v1, const version_t *v2) {
+	return vt_gt(v2, v1);
+}
+
+int vt_le(const version_t *v1, const version_t *v2) {
+	int rv = vt_eq(v1, v2);
+	if (rv == -1) return -1;
+	if (rv == 1) return 1;
+	return vt_lt(v1, v2);
+}
+
+int v_eq(const char *version1, const char *version2) {
+	CONVERT_VERSION_STRINGS
+	int rv = vt_eq(v1, v2);
+COMP_END
+}
+
+int v_ne(const char *version1, const char *version2) {
+	CONVERT_VERSION_STRINGS
+	int rv = vt_ne(v1, v2);
+COMP_END
+}
+
+int v_gt(const char *version1, const char *version2) {
+	CONVERT_VERSION_STRINGS
+	int rv = vt_gt(v1, v2);
+COMP_END
+}
+
+int v_ge(const char *version1, const char *version2) {
+	CONVERT_VERSION_STRINGS
+	int rv = vt_ge(v1, v2);
+COMP_END
+}
+
+int v_lt(const char *version1, const char *version2) {
+	return v_gt(version2, version1);
+}
+
+int v_le(const char *version1, const char *version2) {
+	CONVERT_VERSION_STRINGS
+	int rv = vt_le(v1, v2);
+COMP_END
+}
+
+int main(char *argv[], int argc) {
+	version_t *v = str_to_ver("2.2.35.4");
+	printf("sections: %d\n", v->num_sections);
+	for (int x=0;x<v->num_sections;x++) printf("%d\n", v->sections[x]);
+	free_version(v);
+
+	assert(v_eq("2.13.4.2.4", "4.2.13") == 0);
+	assert(v_eq("2.13.4.2.4", "2.2.13") == 0);
+	assert(v_eq("2.13.4.2.4", "2.13.4") == 0);
+	assert(v_eq("2.13.4", "2.13.4.2.4") == 0);
+	assert(v_eq("2.13.4", "2.13.4") == 1);
+	assert(v_eq("2.13.4", "2.13.2") == 0);
+
+	assert(v_ne("2.13.4.2.4", "4.2.13") == 1);
+	assert(v_ne("2.13.4.2.4", "2.2.13") == 1);
+	assert(v_ne("2.13.4.2.4", "2.13.4") == 1);
+	assert(v_ne("2.13.4", "2.13.4.2.4") == 1);
+	assert(v_ne("2.13.4", "2.13.4") == 0);
+	assert(v_ne("2.13.4", "2.13.2") == 1);
+
+	assert(v_gt("2.13.4.2.4", "4.2.13") == 0);
+	assert(v_gt("2.13.4.2.4", "2.2.13") == 1);
+	assert(v_gt("2.13.4.2.4", "2.13.4") == 1);
+	assert(v_gt("2.13.4", "2.13.4.2.4") == 0);
+	assert(v_gt("2.13.4", "2.13.4") == 0);
+	assert(v_gt("2.13.4", "2.3.4") == 1);
+
+	assert(v_ge("2.13.4.2.4", "4.2.13") == 0);
+	assert(v_ge("2.13.4.2.4", "2.2.13") == 1);
+	assert(v_ge("2.13.4.2.4", "2.13.4") == 1);
+	assert(v_ge("2.13.4", "2.13.4.2.4") == 0);
+	assert(v_ge("2.13.4", "2.13.4") == 1);
+	assert(v_ge("2.13.4", "2.3.4") == 1);
+
+	assert(v_lt("2.13.4.2.4", "4.2.13") == 1);
+	assert(v_lt("2.13.4.2.4", "2.2.13") == 0);
+	assert(v_lt("2.13.4.2.4", "2.13.4") == 0);
+	assert(v_lt("2.13.4", "2.13.4.2.4") == 1);
+	assert(v_lt("2.13.4", "2.13.4") == 0);
+	assert(v_lt("2.3.4", "2.13.4") == 1);
+
+	assert(v_le("2.13.4.2.4", "4.2.13") == 1);
+	assert(v_le("2.13.4.2.4", "2.2.13") == 0);
+	assert(v_le("2.13.4.2.4", "2.13.4") == 0);
+	assert(v_le("2.13.4", "2.13.4.2.4") == 1);
+	assert(v_le("2.13.4", "2.13.4") == 1);
+	assert(v_le("2.3.4", "2.13.4") == 1);
+}
